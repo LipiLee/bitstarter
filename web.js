5cb3ca340ab61b8f77586bacf8c65d1e2524381f
Lipi C.H. Lee <lipisoft@gmail.com>
2013-07-13 17:06:23 +0000

    Change web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var contents = fs.readFileSync('index.html');
+var buffer = new Buffer(contents);
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
